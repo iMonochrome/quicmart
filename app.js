e1bb2ef7a49ae1cd0b28a7ba4f078862d73d31e1
Monochrome <bakadwite@gmail.com>
2019-11-15 06:19:52 +0700

    add all

---
+++ app.js
@@ -0,0 +1,8 @@
+function showModal() {
+  let modal = document.getElementById('modal');
+  modal.classList.toggle('active')
+}
+function closeModal() {
+  let modal = document.getElementById('modal');
+  modal.classList.remove('active')
+}
